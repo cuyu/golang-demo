27d91372f91840f8b2dd027b0f28d902ac94a201
cuyu <icyarm@qq.com>
2019-05-08 16:16:41 +0800

    Add code for goroutie

--- main/12-readers.go
+++ main/12-readers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"golang.org/x/tour/reader"
+	"io"
+	"os"
+	"strings"
 )
 
 type MyReader struct{}
@@ -11,6 +14,32 @@ func (MyReader) Read(b []byte) (int, error) {
 	return 1, nil
 }
 
+// A common pattern is an io.Reader that wraps another io.Reader, modifying the stream in some way.
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r *rot13Reader) Read(b []byte) (int, error) {
+	length, err := r.r.Read(b)
+	for i := range b {
+		switch {
+		case b[i] < 'n' && b[i] >= 'a':
+			b[i] += 13
+		case b[i] >= 'n' && b[i] <= 'z':
+			b[i] -= 13
+		case b[i] < 'N' && b[i] >= 'A':
+			b[i] += 13
+		case b[i] >= 'N' && b[i] <= 'Z':
+			b[i] -= 13
+		}
+	}
+	return length, err
+}
+
 func main() {
 	reader.Validate(MyReader{})
+	// Another example
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
 }
---
+++ main/14-goroutines.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		time.Sleep(100 * time.Millisecond)
+	}
+	c <- sum // send sum to c
+}
+
+func fib(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c) // close the channel
+}
+
+func fibSelect(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			// channel c may get more results if it has buffer (has buffer means it is ready and can be selected)
+			fmt.Printf("put result %s to channel\n", strconv.Itoa(x))
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	// A goroutine is a lightweight thread managed by the Go runtime. Format is `go func(x,y,z)`
+	// Note the evaluation of `f`, `x`, `y`, and `z` happens in the current goroutine and the execution of `f` happens in the new goroutine.
+	// Goroutines run in the same address space, so access to shared memory must be synchronized (like use `channel`).
+	go say("world")
+	say("hello")
+
+	// Channels are a typed conduit through which you can send and receive values with the channel operator, <-.
+	// (The data flows in the direction of the arrow.)
+	// By default, sends and receives block until the other side is ready. This allows goroutines to synchronize without explicit locks or condition variables.
+	// (channel有点像是Python中的Queue，只不过Go中从底层就支持并优化了，注意下面的x和y的值并不是确定的，要看哪个goroutine先执行完成)
+	s := []int{7, 2, 8, -9, 4, 0}
+	// Channels can be buffered. Provide the buffer length as the second argument to `make`.
+	// (类似于Python中Queue的max size，buffer满了会block要进入channel的goroutine)
+	c := make(chan int, 2)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+	fmt.Println(x, y, x+y)
+
+	// A sender can close a channel to indicate that no more values will be sent.
+	// The loop for i := range c receives values from the channel repeatedly until it is closed.
+	// (Closing is only necessary when the receiver must be told there are no more values coming, such as to terminate a range loop.)
+	d := make(chan int, 10)
+	go fib(cap(d), d)
+	for i := range d {
+		fmt.Println(i)
+	}
+
+	// A select blocks until one of its cases can run, then it executes that case. It chooses one at random if multiple are ready.
+	e := make(chan int, 5)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-e)
+		}
+		quit <- 0
+	}()
+	fibSelect(e, quit)
+
+	// The default case in a select is run if no other case is ready.
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
